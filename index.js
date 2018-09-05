08b5cb2ab6719a9071bc05b92efbfdae873c6bdb
Furkan Tunalı <mail@furkantunali.com>
2018-09-05 16:44:18 +0200

    add @nod/webpack-devops-config, upgrade babel=>7, webpack=>4

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const getOrSetBuildTarget = () => {
   return process.env.BUILD_TARGET
 }
 
-const isProduction = getOrSetEnv() === 'production'
-const isTest = getOrSetBuildTarget() === 'test'
+const isProduction = () => getOrSetEnv() === 'production'
+const isTest = () => getOrSetEnv() === 'test'
 
 const browsers = ['last 2 versions']
 const node = 'current'
@@ -22,58 +22,73 @@ const availableTargets = {
   client: { browsers },
   universal: { node, browsers }
 }
-const targets = availableTargets[process.env.BUILD_TARGET]
+const targets = () => availableTargets[getOrSetBuildTarget()]
 
-const flowRuntimePlugin = [
-  require.resolve('babel-plugin-flow-runtime'),
-  { annotate: true, assert: false }
-]
+// const flowRuntimePlugin = [
+//   require.resolve('babel-plugin-flow-runtime'),
+//   { annotate: true, assert: false }
+// ]
 
-const devPlugins = isProduction
-  ? []
-  : [require.resolve('babel-plugin-ramda'), flowRuntimePlugin]
+const devPlugins = () =>
+  isProduction() ? [] : [require.resolve('babel-plugin-ramda')]
+// : [require.resolve('babel-plugin-ramda'), flowRuntimePlugin]
 
-const commonPlugins = [
+const commonPlugins = () => [
   require.resolve('babel-plugin-add-react-displayname'),
-  require.resolve('babel-plugin-add-module-exports'),
+  // require.resolve('babel-plugin-add-module-exports'),
+  require.resolve('@babel/plugin-proposal-class-properties'),
+  [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+  require.resolve('@babel/plugin-proposal-export-default-from'),
+  require.resolve('@babel/plugin-proposal-export-namespace-from'),
+  require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
+  require.resolve('@babel/plugin-proposal-optional-chaining'),
   [
-    require.resolve('babel-plugin-transform-runtime'),
+    require.resolve('@babel/plugin-proposal-pipeline-operator'),
+    { proposal: 'minimal' }
+  ],
+  require.resolve('@babel/plugin-proposal-throw-expressions'),
+  require.resolve('@babel/plugin-syntax-dynamic-import'),
+  require.resolve('@babel/plugin-transform-react-jsx'),
+  require.resolve('@babel/plugin-transform-regenerator'),
+  [
+    require.resolve('@babel/plugin-transform-runtime'),
     {
       helpers: false,
-      polyfill: false,
       regenerator: true
     }
   ]
 ]
-const devPresets = isProduction
-  ? []
-  : [require.resolve('babel-preset-flow-runtime')]
 
-const modules =
-  process.env.BABEL_ENV_MODULES !== true || isTest !== true
-    ? {}
-    : { modules: true }
+const devPresets = () =>
+  isProduction() ? [] : [require.resolve('babel-preset-flow-runtime')]
+// : [require.resolve('babel-preset-flow-runtime')]
 
-const commonPresets = [
-  require.resolve('babel-preset-react-app'),
-  require.resolve('babel-preset-stage-1'),
-  require.resolve('babel-preset-react'),
+const modules = () =>
+  process.env.BABEL_ENV_MODULES === true || isTest() === true ? {} : {}
+
+const commonPresets = () => [
+  // require.resolve('babel-preset-react-app'),
+  require.resolve('@babel/preset-react'),
+  require.resolve('@babel/preset-flow'),
   [
-    require.resolve('babel-preset-env'),
+    require.resolve('@babel/preset-env'),
     Object.assign(
       {},
       {
-        targets,
+        shippedProposals: true,
+        targets: targets(),
         useBuiltIns: 'usage'
       },
-      modules
+      modules()
     )
   ]
 ]
 
-const preset = {
-  plugins: devPlugins.concat(commonPlugins),
-  presets: devPresets.concat(commonPresets)
+function preset() {
+  return {
+    plugins: devPlugins().concat(commonPlugins()),
+    presets: devPresets().concat(commonPresets())
+  }
 }
 
 module.exports = preset
--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
 /* eslint no-unused-expressions:0, better/explicit-return:0, fp/no-nil:0, fp/no-mutation:0, import/no-commonjs:0, fp/no-unused-expression:0 */
 
-import { plugins, presets } from './index'
+import preset from "."
+
+const { plugins, presets } = preset()
 
 describe('@nod/babel-preset-nod', () => {
   test('contains plugins', () => {
